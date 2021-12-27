d3286132b5300a16d56832ed0bd4af018abbb65f
jana-alam <janaalam2929@gmail.com>
2021-12-27 07:23:28 +0600

    firebase, tailwind, reat-router, headless ui , heroincons installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZpqn-MyRiv1Mz8klq8q9SoVWApoLNmZ4",
+  authDomain: "time-keeper-shop.firebaseapp.com",
+  projectId: "time-keeper-shop",
+  storageBucket: "time-keeper-shop.appspot.com",
+  messagingSenderId: "291228529330",
+  appId: "1:291228529330:web:919f32e9734a2c81264fab",
+};
+
+export default firebaseConfig;
---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-2xl text-teal-600">Home page</div>;
+};
+
+export default Home;
